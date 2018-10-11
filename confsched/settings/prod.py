c0c2e3bb07e965ce1418d9e81fbcdf35542316f4
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2018-10-11 12:01:12 -0700

    Configure email for local development

--- confsched/settings/prod.py
+++ confsched/settings/prod.py
@@ -2,6 +2,13 @@ from .base import *
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_PORT = os.environ['EMAIL_PORT']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+
+DEFAULT_FROM_EMAIL = 'noreply@confsched.ernsthaagsman.com'
+
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
