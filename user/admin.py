59991c3417df3c739b49136f1b28bb0ffa404c65
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2018-10-11 13:43:53 -0700

    Finish user add flow

--- user/admin.py
+++ user/admin.py
@@ -1,37 +1,18 @@
 from django import forms
 from django.contrib import admin
+from django.utils.crypto import get_random_string
 from django.utils.translation import gettext, gettext_lazy as _
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.forms import ReadOnlyPasswordHashField, PasswordResetForm
 
 from user.models import ConfSchedUser
 
 
 class UserCreationForm(forms.ModelForm):
-    """A form for creating new users. Includes all the required
-    fields, plus a repeated password."""
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
 
     class Meta:
         model = ConfSchedUser
-        fields = ('email',)
-
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
-
-    def save(self, commit=True):
-        # Save the provided password in hashed format
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
+        fields = ('email', 'first_name', 'last_name')
 
 
 class UserChangeForm(forms.ModelForm):
@@ -74,11 +55,26 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2')}
+            'fields': ('email', 'first_name', 'last_name')}
         ),
     )
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
+
+    def save_model(self, request, obj, form, change):
+        # We have to reset the user's password right after creating it
+        obj.set_password(get_random_string())
+
+        super().save_model(request, obj, form, change)
+
+        reset_form = PasswordResetForm({'email': obj.email})
+        assert reset_form.is_valid()
+        reset_form.save(
+            request=request,
+            use_https=request.is_secure(),
+            subject_template_name='user/registered_subject.txt',
+            email_template_name='user/registered.html',
+        )
 
 
 admin.site.register(ConfSchedUser, UserAdmin)
