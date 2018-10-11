390ed1941b8161fc23460c811ec350199bc875fc
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2018-10-11 11:53:03 -0700

    Implement Django authentication templates

--- confsched/urls.py
+++ confsched/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include, re_path
 
 from scheduler.views import TalkListView, TalkReprioritizeView, TalkDeleteView, TalkAddView, IndexView
@@ -28,5 +29,18 @@ urlpatterns = [
         name='talk_reprioritize'),
     path('<slug:event_slug>/talks/<int:talk_id>/delete', TalkDeleteView.as_view(), name='talk_delete'),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             success_url='/',
+             post_reset_login=True
+         ),
+         name='password_reset_confirm'),
 ]
