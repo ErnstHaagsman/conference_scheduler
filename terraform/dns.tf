11d0402f7c8b680d17720d7e3936f1e6fffe2713
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2018-10-14 13:47:16 -0700

    Add initial Terraform and Ansible configuration

---
+++ terraform/dns.tf
@@ -0,0 +1,11 @@
+data "aws_route53_zone" "main" {
+  name = "confsched.ernsthaagsman.com."
+}
+
+resource "aws_route53_record" "root" {
+  zone_id = "${data.aws_route53_zone.main.zone_id}"
+  name = "${data.aws_route53_zone.main.name}"
+  type = "A"
+  ttl = "30"
+  records = ["${aws_instance.web.public_ip}"]
+}
---
+++ terraform/instances.tf
@@ -0,0 +1,61 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_key_pair" "key" {
+  key_name = "confsched_key"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+resource "aws_instance" "web" {
+  ami = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  key_name = "${aws_key_pair.key.key_name}"
+
+  vpc_security_group_ids = [
+    "${aws_security_group.web_sg.id}"
+  ]
+
+  tags = {
+    role = "web"
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name = "Security group for web boxes"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
