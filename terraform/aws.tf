50f6b886ece9d98d571c0c7bbd8a58766ab92469
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2018-10-16 17:39:06 -0700

    Add gunicorn basic configuration

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,3 +1,8 @@
 provider "aws" {
+  alias = "aws-eu"
   region = "eu-central-1"
+}
+
+provider "aws" {
+  region = "us-west-2"
 }
--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,8 +1,10 @@
 data "aws_route53_zone" "main" {
+  provider = "aws.aws-eu"
   name = "confsched.ernsthaagsman.com."
 }
 
 resource "aws_route53_record" "root" {
+  provider = "aws.aws-eu"
   zone_id = "${data.aws_route53_zone.main.zone_id}"
   name = "${data.aws_route53_zone.main.name}"
   type = "A"
