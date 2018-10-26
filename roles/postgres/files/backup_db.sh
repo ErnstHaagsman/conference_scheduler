02fb016530a65f5664bb8fd2ac1314e93e9fb4a2
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2018-10-26 13:06:31 +0200

    Absolute paths in cron scripts
    S3 upload fails silently with relative path

--- roles/postgres/files/backup_db.sh
+++ roles/postgres/files/backup_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pg_dump "$1" --format=custom -f "/var/backups/database/$1-latest"
-aws s3 cp "/var/backups/database/$1-latest" "s3://$2/$1-$(date -Iminutes)"
+/usr/bin/pg_dump "$1" --format=custom -f "/var/backups/database/$1-latest"
+/usr/local/bin/aws s3 cp "/var/backups/database/$1-latest" "s3://$2/$1-$(date -Iminutes)"
