cb6729e785d5565d75ef7587f06d892b03db6e7d
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2018-10-30 20:08:58 +0100

    Make static files instance specific
    Also, don't generate them as root

--- src/confsched/settings/base.py
+++ src/confsched/settings/base.py
@@ -107,7 +107,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = '/var/www/static'
+STATIC_ROOT = os.environ['STATIC_ROOT']
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
