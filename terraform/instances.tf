eae85ce850177d4334844528f8d010ac163ca981
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2018-10-30 19:46:23 +0100

    Switch to HTTPS

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -53,6 +53,13 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 443
+    protocol = "tcp"
+    to_port = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port = 0
